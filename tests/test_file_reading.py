e7cfef41dfe8271d551fe40d4455e5de005cbf96
Srinivas <sm1043@gmail.com>
2024-01-05 17:12:18 +0530

    Second commit

---
+++ tests/test_file_reading.py
@@ -0,0 +1,14 @@
+from nlp_pipeline.embeddings import DataReader
+def main():
+    file_path = 'data/textfiletesting.txt' 
+    # Create an instance of the DataReader class
+    data_reader = DataReader(file_path)
+
+    # Use the class method to read data from the file
+    data = data_reader.read_data_from_file()
+    
+    print("Content of the file:")
+    print(data)
+
+if __name__ == "__main__":
+    main()
